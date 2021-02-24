aac85eaf5befd3d9b22d1639bf744c3b1b632830
hanbee <sonhanbi1002@gmail.com>
2021-02-24 23:44:12 +0900

    index.js 등록 버튼 클릭 시 동작 오류 수정

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,7 +1,7 @@
 var main = {
     init : function () {
       var _this = this;
-      $('btn-save').on('click', function () {
+      $('#btn-save').on('click', function () {
           _this.save();
       });
     },
